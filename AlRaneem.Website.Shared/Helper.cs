af82574a96a534357c179c110f5ea59a0cd25324
a.ahmed <a.ahmed@pioneersconsult.com>
2025-05-11 14:03:07 +0300

    shared

---
+++ AlRaneem.Website.Shared/Helper.cs
@@ -0,0 +1,10 @@
+﻿namespace AlRaneem.Website.Shared
+{
+    public static class Helper
+    {
+        public static byte[] ConvertImageToByteArray(string imagePath)
+        {
+            return File.ReadAllBytes(imagePath);
+        }
+    }
+}
